176c8381919c35e8b900694f2e4bfe623a7c0c5f
ndurbin <nick.durbin@gmail.com>
2019-08-29 09:10:00 -0400

    Removing some extraspace.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-
+import { Route } from 'react-router-dom';
 import SavedList from './Movies/SavedList';
+import Movie from './Movies/Movie'
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -12,7 +13,7 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Route path='/movie' component={Movie}/>
     </div>
   );
 };
