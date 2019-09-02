6360198b42cafd42857bdc691eba251a69d945c8
ndurbin <nick.durbin@gmail.com>
2019-09-02 06:48:49 -0400

    Made the savedList a link to the saved movies.

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -22,10 +22,10 @@ const Movie = (props) => {
   },[props.match.params.id]);
   
   // Uncomment this only when you have moved on to the stretch goals
-  // const saveMovie = () => {
-  //   const addToSavedList = props.addToSavedList;
-  //   addToSavedList(movie)
-  // }
+  const saveMovie = () => {
+    const addToSavedList = props.addToSavedList;
+    addToSavedList(movie)
+  }
 
   if (!movie) {
     return <div>Loading movie information...</div>;
@@ -33,7 +33,7 @@ const Movie = (props) => {
 
   return (
     <div className="save-wrapper">
-        <MovieCard key={movie.id} movie={movie} />
+      <MovieCard key={movie.id} movie={movie} stars={movie.stars}/>
       <div className="save-button">Save</div>
     </div>
   );
